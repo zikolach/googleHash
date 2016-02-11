0d49fc77406eadc9ae814e8fdf15e2252220daef
Nikolay Kushin <nikolay@indoo.rs>
2016-02-11 17:00:12 +0100

    try to find which is better - horizontal or vertical lines

--- src/main/scala/GoogleHash.scala
+++ src/main/scala/GoogleHash.scala
@@ -3,6 +3,7 @@ import java.nio.file.{Files, Paths}
 
 import scala.annotation.tailrec
 import scala.io.Source
+import scala.language.postfixOps
 
 object GoogleHash {
 
@@ -20,7 +21,7 @@ object GoogleHash {
     cmds.reverse
   }
 
-  def p1(i: Int, str: String): List[String] = {
+  def findLines(i: Int, str: String): List[(Int, Int, Int, Int)] = {
     @tailrec
     def findAll(acc: List[(Int, Int)], from: Int): List[(Int, Int)] = {
       val s = str.indexOf("#", from)
@@ -32,24 +33,46 @@ object GoogleHash {
       }
     }
     findAll(List.empty, 0).map { case (s, l) =>
-      s"PAINT_LINE $i $s $i ${s + l - 1}"
+      (i, s, i, s + l - 1)
+    }
+  }
+
+  def p1(i: Int, str: String): List[String] = {
+    findLines(i, str).map { case (r1, c1, r2, c2) =>
+      s"PAINT_LINE $r1 $c1 $r2 $c2"
     } reverse
   }
 
-  def main(args: Array[String]): Unit = {
-//        val filename = "logo"
-        val filename = "right_angle"
-//    val filename = "learn_and_teach"
-    val tmp = Source.fromFile(Paths.get(s"$filename.in").toFile).getLines().toList
-
-    val data = tmp.head.split(' ')
-    val rows = Integer.parseInt(data(0))
-    val cols = Integer.parseInt(data(1))
-
-    var i = 0
-    val commands = tmp.tail.zipWithIndex.flatMap {
+  def processLines1(lines: List[String]): List[String] = {
+    lines.tail.zipWithIndex.flatMap {
       case (line, index) => p1(index, line)
     }
+  }
+
+  def processLines2(lines: List[String]): List[String] = {
+    val h = lines.tail.zipWithIndex.flatMap {
+      case (line, index) => findLines(index, line)
+    }
+    val v = lines.tail.transpose.zipWithIndex.flatMap {
+      case (line, index) => findLines(index, line.mkString)
+    }
+    val tuples = if (v.length < h.length) {
+      v.map(t => (t._2, t._1, t._4, t._3))
+    } else {
+      h
+    }
+    tuples map { case (r1, c1, r2, c2) =>
+      s"PAINT_LINE $r1 $c1 $r2 $c2"
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    // val filename = "logo"
+    // val filename = "right_angle"
+    val filename = "learn_and_teach"
+    val tmp = Source.fromFile(Paths.get(s"$filename.in").toFile).getLines().toList
+
+    val commands = processLines2(tmp)
     commands.foreach(println)
 
     Files.write(Paths.get(s"$filename.out"), (commands.length.toString :: commands).mkString("\n").getBytes(StandardCharsets.UTF_8))
