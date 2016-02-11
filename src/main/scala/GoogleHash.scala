5cdd2689214fa81b1d7dbcda2c68c30bbc3a8ce3
Nikolay Kushin <nikolay@indoo.rs>
2016-02-11 16:00:05 +0100

    2nd solution

--- src/main/scala/GoogleHash.scala
+++ src/main/scala/GoogleHash.scala
@@ -1,6 +1,7 @@
 import java.nio.charset.StandardCharsets
 import java.nio.file.{Files, Paths}
 
+import scala.annotation.tailrec
 import scala.io.Source
 
 object GoogleHash {
@@ -11,10 +12,7 @@ object GoogleHash {
     for (c <- s) {
       val c1 = "" + c
       c1 match {
-        case "#" =>
-          //          Console.println("PAINT_LINE " + i + " " + j + " " + i + " " + j)
-          cmds ::= s"PAINT_LINE $i $j $i $j"
-          co = co + 1
+        case "#" => cmds ::= s"PAINT_LINE $i $j $i $j"
         case _ =>
       }
       j = j + 1
@@ -22,12 +20,26 @@ object GoogleHash {
     cmds.reverse
   }
 
-  var co = 0
+  def p1(i: Int, str: String): List[String] = {
+    @tailrec
+    def findAll(acc: List[(Int, Int)], from: Int): List[(Int, Int)] = {
+      val s = str.indexOf("#", from)
+      val e = str.indexOf(".", s)
+      (s, e) match {
+        case (-1, _) => acc
+        case (_, -1) => (s, str.length - s) :: acc
+        case _ => findAll((s, e - s) :: acc, e)
+      }
+    }
+    findAll(List.empty, 0).map { case (s, l) =>
+      s"PAINT_LINE $i $s $i ${s + l - 1}"
+    } reverse
+  }
 
   def main(args: Array[String]): Unit = {
-    //    val filename = "logo"
-    //    val filename = "right_angle"
-    val filename = "learn_and_teach"
+//        val filename = "logo"
+        val filename = "right_angle"
+//    val filename = "learn_and_teach"
     val tmp = Source.fromFile(Paths.get(s"$filename.in").toFile).getLines().toList
 
     val data = tmp.head.split(' ')
@@ -36,7 +48,7 @@ object GoogleHash {
 
     var i = 0
     val commands = tmp.tail.zipWithIndex.flatMap {
-      case (line, index) => p(index, line)
+      case (line, index) => p1(index, line)
     }
     commands.foreach(println)
 
